12c82923cbca76482161d000caca28130e9c8c14
Rahul Chandan <luhar.nadnahc@gmail.com>
2018-04-14 18:13:17 -0700

    Added further functionality, able to calculate smoothness

--- gtToolbox.py
+++ gtToolbox.py
@@ -1,6 +1,9 @@
 import sys
+import csv
 import numpy as np
 from scipy.special import factorial
+import matplotlib.pyplot as plt
+
 
 ##
 #
@@ -25,11 +28,13 @@ def equal_share(j,k):
 class GameObject:
     def __init__(self, res, act):
         self.num_players = len(act)
-        self.resources = np.array(res)
+        self.resources = res
         self.action_sets = act
         self.dist_rule = None
+        self.lambda_mu = {}
 
     def generate_table(self, dist_rule):
+        print('GameObject:generate_table: using function \'%s\' as distribution rule.' % dist_rule.__name__)
         self.dist_rule = dist_rule
         
         table_shape = [1]
@@ -43,7 +48,7 @@ class GameObject:
             res_idx = np.zeros(self.num_players, dtype=np.int)
             bin_vals = np.zeros(self.num_players)
             for i in range(self.num_players):
-                act_length = len(self.action_sets[i])           
+                act_length = len(self.action_sets[i])
                 act_idx = int(k / divide_param) % act_length
                 res_idx[i] = self.action_sets[i][act_idx]                
                 bin_vals[i] = self.resources[res_idx[i]]
@@ -54,12 +59,12 @@ class GameObject:
                 self.table[k,i] = bin_vals[i] * self.dist_rule(overlap, self.num_players)
             self.table[k,self.num_players] = np.sum(self.resources[np.unique(res_idx)])
 
-    def robust_poa(self):
+    def construct_lambda_mu(self):
         for k in range(np.shape(self.table)[0]):
             s_divide_param = 1
             s = np.zeros(self.num_players, dtype=np.int)
             for i in range(self.num_players):
-                act_length = len(self.action_sets[i])           
+                act_length = len(self.action_sets[i])
                 s[i] = int(k / s_divide_param) % act_length
                 s_divide_param = s_divide_param * act_length
             for l in range(np.shape(self.table)[0]):
@@ -67,7 +72,7 @@ class GameObject:
                 sum_utils = 0
                 s_star = np.zeros(self.num_players, dtype=np.int)
                 for j in range(self.num_players):
-                    act_length_star = len(self.action_sets[j])           
+                    act_length_star = len(self.action_sets[j])
                     s_star[j] = int(l / s_star_divide_param) % act_length_star
                     s_star_divide_param = s_star_divide_param * act_length_star
                 for i, s_star_i in enumerate(s_star):
@@ -77,22 +82,77 @@ class GameObject:
                     reconstruct_param = 1
                     for p in range(self.num_players):
                         m = m + reconstruct_param * tmp[p]
-                        reconstruct_param = reconstruct_param * len(self.action_sets[p])
+                        reconstruct_param = reconstruct_param*len(self.action_sets[p])
                     sum_utils = sum_utils + self.table[m,i]
                 slope = "%.8f" % (self.table[l,self.num_players] / self.table[k,self.num_players])
                 y_int = sum_utils / self.table[k,self.num_players]
-                print("%.8f >= %dL - %dU" % (sum_utils, self.table[l,self.num_players], self.table[k,self.num_players]))
-        return
+                # print("%.8f >= %dL - %dU" % (sum_utils, self.table[l,self.num_players], self.table[k,self.num_players]))
+                if slope in self.lambda_mu:
+                    self.lambda_mu[slope] = np.min([y_int, self.lambda_mu[slope]])
+                else:
+                    self.lambda_mu[slope] = y_int
+                    
+    def robust_poa(self):
+        xMax = 100
+        x = np.arange(0,xMax,0.001)
+        slopesAndYInts = []
+        maxSlope = -1
+        for slope in sorted(self.lambda_mu, key=self.lambda_mu.get):
+            fSlope = float(slope)
+            if fSlope > maxSlope:
+                slopesAndYInts.append((fSlope, self.lambda_mu[slope]))
+                maxSlope = fSlope
+
+        tableOfVals = np.zeros((len(slopesAndYInts), np.size(x)))
+        for idx, (slope,y_int) in enumerate(slopesAndYInts):
+            tableOfVals[idx,:] = slope*x-y_int
+        maxVals = np.max(tableOfVals, axis=0)
+        robustPoAs = x/(1+maxVals)
+        bestPoA = np.max(robustPoAs)
+        bestPoAIdx = np.argmax(robustPoAs)
+        lam = bestPoAIdx/np.size(x)*xMax
+        mu = maxVals[bestPoAIdx]
+
+        return (lam, mu, bestPoA)
         
     def print_table(self):
         print('GameObject:print_table():\n', self.table)
 
-def main():
-    g = GameObject(A,B)
+def main(args):
+    res = []
+    act = []
+    
+    try:
+        with open(args[1]) as csvfile:
+            for line in csv.reader(csvfile):
+                act.append(line)
+        with open(args[2]) as csvfile:
+            for line in csv.reader(csvfile):
+                res = line
+    except IndexError:
+        print('Please provide two .csv files to be parsed.')
+        return
+
+    act = [[int(i) for i in act_set] for act_set in act]
+    res = np.array(res, dtype=np.float)
+
+    print('__main__: array \'res\':', res)
+    print('__main__: list \'act\':', act)
+    
+    g = GameObject(res, act)
     g.generate_table(equal_share)
-    g.robust_poa()
-    # g.print_table()
+    g.construct_lambda_mu()
+    print(g.robust_poa())
+    
+    g.generate_table(gairing)
+    g.construct_lambda_mu()
+    print(g.robust_poa())
+    
+    g.generate_table(marginal_contribution)
+    g.construct_lambda_mu()
+    print(g.robust_poa())
+    
     pass
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv))
